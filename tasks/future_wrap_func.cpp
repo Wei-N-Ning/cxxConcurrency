66cb89c7b006923c880d1694c7291a48d802f414
Wei Ning <nwcgnw@gmail.com>
2018-08-11 15:07:02 +1200

    - init proj

---
+++ tasks/future_wrap_func.cpp
@@ -0,0 +1,24 @@
+
+#include <cassert>
+#include <future>
+
+namespace threading_ {
+
+int _compute(int value) {
+    if (value > 0) {
+        return 10;
+    }
+    return -10;
+}
+
+int execute(int value) {
+    std::future<int> fut = std::async(_compute, std::ref(value));
+    return fut.get();
+}
+
+} // namespace threading_
+
+int main(int argc, char **argv) {
+    int result = threading_::execute(-1);
+    assert(result == -10);
+}
--- tasks/test_threadModel.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <cassert>
-#include "threadModel.hpp"
-
-int main(int argc, char **argv) {
-    int result = threading_::execute(-1);
-    assert(result == -10);
-}
