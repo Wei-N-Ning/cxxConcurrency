dcb51816d04bdbd988eeecde2e1862beffd799c1
Wei Ning <wning@wetafx.co.nz>
2018-10-26 10:06:13 +1300

    - add rwlocks

---
+++ rwlocks/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * source:
+ *
+ * C++17 shared mutex
+ * or C++14 shared_timed_mutex
+ *
+ * https://en.cppreference.com/w/cpp/thread/shared_mutex
+ *
+ * Intel's TBB also offers reader_writer_lock.h 
+ *
+ */
+
+
+
+
+
+int main() {
+    return 0;
+}
