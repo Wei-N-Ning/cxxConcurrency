1929e8ebdeb88ca401843a29296c1de0294273dc
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-04-13 19:34:13 +1000

    add vendor scripts

---
+++ vendor/get-hwloc.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# url=https://download.open-mpi.org/release/hwloc/v2.2/hwloc-2.2.0.tar.bz2
+
+set -euo pipefail
+
+# parameters
+version="${1:?missing version!}"
+dest=$(python -c "import os;print(os.path.realpath('${dest:-.}'))")
+
+# static
+thisfile=$(python -c "import os;print(os.path.realpath('${BASH_SOURCE[0]}'))")
+thisdir=$(dirname "${thisfile}")
+
+download_untar() {
+    local filename="hwloc-${version}"
+    local fext="${filename}.tar.bz2"
+    if [[ ! -f "${fext}" ]]; then
+        local downloadurl="https://download.open-mpi.org/release/hwloc/v2.2/${fext}"
+        wget "${downloadurl}"
+    fi
+    if [[ ! -d "${filename}" ]]; then
+        tar -xf "${filename}.tar.bz2"
+    fi
+    echo -n "${filename}"
+}
+
+do_install() {
+    ./configure --prefix="${dest}"
+    make -j8 && make install
+}
+
+cd /tmp
+d=$(download_untar)
+cd "${d}"
+do_install
+
---
+++ vendor/get-jemalloc.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# https://github.com/jemalloc/jemalloc/releases/download/5.2.1/jemalloc-5.2.1.tar.bz2
+
+set -euo pipefail
+
+# parameters
+version="${1:?missing version!}"
+dest=$(python -c "import os;print(os.path.realpath('${dest:-.}'))")
+
+# static
+thisfile=$(python -c "import os;print(os.path.realpath('${BASH_SOURCE[0]}'))")
+thisdir=$(dirname "${thisfile}")
+
+download_untar() {
+    local filename="jemalloc-${version}"
+    local fext="${filename}.tar.bz2"
+    if [[ ! -f "${fext}" ]]; then
+        local downloadurl="https://github.com/jemalloc/jemalloc/releases/download/${version}/${fext}"
+        wget "${downloadurl}"
+    fi
+    if [[ ! -d "${filename}" ]]; then
+        tar -xf "${filename}.tar.bz2"
+    fi
+    echo -n "${filename}"
+}
+
+do_install() {
+    ./configure --prefix="${dest}"
+    make -j8 && make install
+}
+
+cd /tmp
+d=$(download_untar)
+cd "${d}"
+do_install
+
