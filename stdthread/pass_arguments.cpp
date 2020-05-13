cb78bf44108770860c469aa33a64751601054c8a
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-05-13 13:06:20 +1000

    a minimal thread pool built on top of std::thread and deque

--- stdthread/pass_arguments.cpp
+++ stdthread/pass_arguments.cpp
@@ -8,6 +8,9 @@
 
 #include <thread>
 #include <functional>
+#include <vector>
+
+// mastering c++ programming: L5658
 
 TEST_CASE ("pass arguments of prim types") {
     using namespace std;
@@ -17,4 +20,29 @@ TEST_CASE ("pass arguments of prim types") {
     thread t(f, 1, 2);
     t.join();
 
+}
+
+TEST_CASE ("pass-by-reference") {
+    using namespace std;
+
+    vector<int> v{1};
+
+    function<int(vector<int> &)> f = [](vector<int> &v) {
+        if (v.size() == 1) {
+            v[0] = 10;
+            return 1;
+        }
+        return 0;
+    };
+
+    // L5658
+    // thread ctor copies the values passed to it and then passes them to the
+    // thread function with rvalue reference.
+    // this is done to work with move-only types
+    // (can 't pass rvalue to a function that expects a non-constant ref)
+    // use std::ref
+    thread t(f, ref(v));
+    t.join();
+
+    CHECK_EQ(10, v[0]);
 }
---
+++ stdthread/thread_pool.cpp
@@ -0,0 +1,82 @@
+//
+// Created by weining on 13/5/20.
+//
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+
+#include <algorithm>
+#include <deque>
+#include <vector>
+#include <thread>
+
+// mastering c++ programming L5676
+int fib(int n) {
+    if (n == 0 or n == 1) return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
+struct ThreadPool {
+    std::deque<std::thread> pool{};
+    std::size_t max_size{std::thread::hardware_concurrency()};
+
+    ThreadPool() = default;
+    explicit ThreadPool(std::size_t sz): max_size(sz) {}
+
+    template<typename... Ts>
+    void do_work(Ts &&... args) {
+        if (pool.size() < max_size) {
+            pool.emplace_back(std::forward<Ts>(args)...);
+        } else {
+            pool.front().join();
+            pool.pop_front();
+            pool.emplace_back(std::forward<Ts>(args)...);
+        }
+    }
+
+    ~ThreadPool() {
+        while (!pool.empty()) {
+            pool.front().join();
+            pool.pop_front();
+        }
+    }
+};
+
+// --- proof of concept ---
+
+//TEST_CASE ("") {
+//    using namespace std;
+//
+//    deque<thread> pool;
+//    auto max_size{thread::hardware_concurrency()};
+//
+//    vector<int> workload(40, 30);
+//
+//    for_each(
+//        cbegin(workload), cend(workload),
+//        [&pool, max_size](int work) {
+//            if (pool.size() < max_size) {
+//                pool.emplace_back(fib, work);
+//            } else {
+//                pool.front().join();
+//                pool.pop_front();
+//                pool.emplace_back(fib, work);
+//            }
+//        }
+//    );
+//    while (!pool.empty()) {
+//        pool.front().join();
+//        pool.pop_front();
+//    }
+//}
+
+// one core to all cores speed up: 4.771
+TEST_CASE ("deque-based thread pool") {
+    using namespace std;
+    ThreadPool tp;
+
+    vector<int> workload(40, 40);
+    for_each(begin(workload), end(workload),
+             [&tp](int work) { tp.do_work(fib, work); });
+}
