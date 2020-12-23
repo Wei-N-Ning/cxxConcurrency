ee6b0e974c7a7a11dc9b0894e3d01aec563474b1
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-24 10:46:38 +1100

    c++ 17: review and update

---
+++ hardware_cache/keep_apart.cpp
@@ -0,0 +1,11 @@
+//
+// Created by weining on 24/12/20.
+//
+
+// what is `hardware_destructive_interference_size` for?
+// Minimum offset between two objects to avoid false sharing
+// Maximum size of contiguous memory to promote true sharing
+
+// see: https://en.cppreference.com/w/cpp/thread/hardware_destructive_interference_size
+
+// also read: c++ 17 the complete guide P/302
